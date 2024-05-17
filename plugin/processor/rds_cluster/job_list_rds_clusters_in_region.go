6c9599128c28a3ff376ae4113324cd037ba93832
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-05-17 20:53:07 +0400

    fix: Fixes login required

--- plugin/processor/rds_cluster/job_list_rds_clusters_in_region.go
+++ plugin/processor/rds_cluster/job_list_rds_clusters_in_region.go
@@ -2,7 +2,13 @@ package rds_cluster
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go-v2/service/rds/types"
+	"github.com/google/uuid"
+	"github.com/kaytu-io/kaytu/pkg/utils"
+	"github.com/kaytu-io/kaytu/preferences"
+	"github.com/kaytu-io/plugin-aws/plugin/kaytu"
 	preferences2 "github.com/kaytu-io/plugin-aws/plugin/preferences"
+	"github.com/kaytu-io/plugin-aws/plugin/version"
 )
 
 type ListRDSClustersInRegionJob struct {
@@ -49,52 +55,62 @@ func (j *ListRDSClustersInRegionJob) Run() error {
 			if j.processor.lazyloadCounter.Get() > j.processor.configuration.RDSLazyLoad {
 				oi.LazyLoadingEnabled = true
 			}
-			//TODO-Saleh
-			//
-			//var clusterType kaytu.AwsRdsClusterType
-			//multiAZ := oi.Instance.MultiAZ != nil && *oi.Instance.MultiAZ
-			//readableStandbys := oi.Instance.ReplicaMode == types.ReplicaModeOpenReadOnly
-			//if multiAZ && readableStandbys {
-			//	clusterType = kaytu.AwsRdsClusterTypeMultiAzTwoInstance
-			//} else if multiAZ {
-			//	clusterType = kaytu.AwsRdsClusterTypeMultiAzOneInstance
-			//} else {
-			//	clusterType = kaytu.AwsRdsClusterTypeSingleInstance
-			//}
-			//
-			//reqID := uuid.New().String()
-			//req := kaytu.AwsRdsWastageRequest{
-			//	RequestId:      &reqID,
-			//	CliVersion:     &version.VERSION,
-			//	Identification: j.processor.identification,
-			//	Instance: kaytu.AwsRds{
-			//		HashedInstanceId:                   utils.HashString(*oi.Instance.DBInstanceIdentifier),
-			//		AvailabilityZone:                   *oi.Instance.AvailabilityZone,
-			//		InstanceType:                       *oi.Instance.DBInstanceClass,
-			//		Engine:                             *oi.Instance.Engine,
-			//		EngineVersion:                      *oi.Instance.EngineVersion,
-			//		LicenseModel:                       *oi.Instance.LicenseModel,
-			//		BackupRetentionPeriod:              oi.Instance.BackupRetentionPeriod,
-			//		ClusterType:                        clusterType,
-			//		PerformanceInsightsEnabled:         *oi.Instance.PerformanceInsightsEnabled,
-			//		PerformanceInsightsRetentionPeriod: oi.Instance.PerformanceInsightsRetentionPeriod,
-			//		StorageType:                        oi.Instance.StorageType,
-			//		StorageSize:                        oi.Instance.AllocatedStorage,
-			//		StorageIops:                        oi.Instance.Iops,
-			//	},
-			//	Metrics:     oi.Metrics,
-			//	Region:      oi.Region,
-			//	Preferences: preferences.Export(oi.Preferences),
-			//	Loading:     true,
-			//}
-			//if oi.Instance.StorageThroughput != nil {
-			//	floatThroughput := float64(*oi.Instance.StorageThroughput)
-			//	req.Instance.StorageThroughput = &floatThroughput
-			//}
-			//_, err := kaytu.RDSInstanceWastageRequest(req, j.processor.kaytuAcccessToken)
-			//if err != nil {
-			//	return err
-			//}
+
+			reqID := uuid.New().String()
+			var reqInstances []kaytu.AwsRds
+			for _, i := range oi.Instances {
+				var clusterType kaytu.AwsRdsClusterType
+				multiAZ := i.MultiAZ != nil && *i.MultiAZ
+				readableStandbys := i.ReplicaMode == types.ReplicaModeOpenReadOnly
+				if multiAZ && readableStandbys {
+					clusterType = kaytu.AwsRdsClusterTypeMultiAzTwoInstance
+				} else if multiAZ {
+					clusterType = kaytu.AwsRdsClusterTypeMultiAzOneInstance
+				} else {
+					clusterType = kaytu.AwsRdsClusterTypeSingleInstance
+				}
+
+				rdsInstance := kaytu.AwsRds{
+					HashedInstanceId:                   utils.HashString(*i.DBInstanceIdentifier),
+					AvailabilityZone:                   *i.AvailabilityZone,
+					InstanceType:                       *i.DBInstanceClass,
+					Engine:                             *i.Engine,
+					EngineVersion:                      *i.EngineVersion,
+					LicenseModel:                       *i.LicenseModel,
+					BackupRetentionPeriod:              i.BackupRetentionPeriod,
+					ClusterType:                        clusterType,
+					PerformanceInsightsEnabled:         *i.PerformanceInsightsEnabled,
+					PerformanceInsightsRetentionPeriod: i.PerformanceInsightsRetentionPeriod,
+					StorageType:                        i.StorageType,
+					StorageSize:                        i.AllocatedStorage,
+					StorageIops:                        i.Iops,
+				}
+				if i.StorageThroughput != nil {
+					floatThroughput := float64(*i.StorageThroughput)
+					rdsInstance.StorageThroughput = &floatThroughput
+				}
+
+				reqInstances = append(reqInstances, rdsInstance)
+			}
+
+			req := kaytu.AwsClusterWastageRequest{
+				RequestId:      &reqID,
+				CliVersion:     &version.VERSION,
+				Identification: j.processor.identification,
+				Cluster: kaytu.AwsRdsCluster{
+					HashedClusterId: utils.HashString(*oi.Cluster.DBClusterIdentifier),
+					Engine:          *oi.Cluster.Engine,
+				},
+				Instances:   reqInstances,
+				Metrics:     oi.Metrics,
+				Region:      oi.Region,
+				Preferences: preferences.Export(oi.Preferences),
+				Loading:     true,
+			}
+			_, err := kaytu.RDSClusterWastageRequest(req, j.processor.kaytuAcccessToken)
+			if err != nil {
+				return err
+			}
 		}
 
 		// just to show the loading
